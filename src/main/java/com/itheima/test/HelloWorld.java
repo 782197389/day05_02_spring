e248ac3444db4639d4aa948a3ea38e793dfbb17d
MrLi <782197389@qq.com>
2019-06-09 19:28:30 +0800

    Initial commit

---
+++ src/main/java/com/itheima/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itheima.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int b=100;
+        System.out.println("HelloWorld!!");
+        System.out.println("HelloWorld!!");
+        System.out.println("HelloWorld!!");
+    }
+}
